5f28356364c554ef7ab8bae2dfd7a97b099651e1
lomarco <ororr069@gmail.com>
2025-12-12 23:34:33 +0700

    Comment hardware-confifuration.nix

--- configuration.nix
+++ configuration.nix
@@ -25,10 +25,7 @@
   time.timeZone = "Asia/Novosibirsk";
 
   i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    # font = "Lat2-Terminus16";
-    useXkbConfig = true;
-  };
+  console.useXkbConfig = true;
 
 
   nix = {
